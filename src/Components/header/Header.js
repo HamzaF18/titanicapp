bc996e0e470d705e09a508681e9145a8ae534c08
HamzaF18 <40198734@hruc.ac.uk>
2023-10-17 15:55:54 +0100

    init

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,13 +1,18 @@
-import "./Header.css"
+import "./Header.css";
+import logo from "../../Assests/logo.png";
 
-const Header =() =>{
-    return(
+const Header = () => {
+    return (
         <div className="header-container">
+            <div className="logo-container">
+                <a href="/home"> <img className="Logo" src={logo} /></a>
+            </div>
             <h1>Titanic Fitness Gym</h1>
             <ul className="nav-area">
-            <button className="nav-button"><a href="http://localhost:3000/register">Register</a></button>
-            <button className="nav-button"><a href="http://localhost:3000/login">Sign in</a></button>
+                <button className="nav-button"><a href="http://localhost:3000/register">Register</a></button>
+                <button className="nav-button"><a href="http://localhost:3000/login">Sign in</a></button>
             </ul>
+
         </div>
     )
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,11 +2,8 @@ import "./Home.css"
 
 const Home =() =>{
     return(
-        <div>
-
-          <img  className="image-container" src="https://images.pexels.com/photos/1954524/pexels-photo-1954524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <div className="home-content">
           
-           
         </div>
     )
 }
