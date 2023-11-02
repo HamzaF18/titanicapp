e7a637d84ebe52311216449fc7a747f986be8ff1
HamzaF18 <40198734@hruc.ac.uk>
2023-11-02 13:50:51 +0000

    init

--- src/views/About.js
+++ src/views/About.js
@@ -1,12 +1,51 @@
 import "./About.css";
+import { useNavigate } from "react-router-dom";
+
+
+const NavButton = ({ text, path }) => {
+  const navigate = useNavigate()
+  return (
+    <button className="about-button" onClick={() => navigate(path)}>
+      {text}
+    </button>
+  )
+}
+
+
+
+
+
+
+
+
 
 const About = () => {
   return (
-    <div className="background-image ">
-      <div className="about-title">
-        <h1>We are Titanic Fitness Gym Group</h1>
+    
+    <div className="background-image">
+      <div className="about">
+        <div className="about-title">
+          <h1>We are Titanic Fitness Gym Group</h1>
+        </div>
+        <br></br>
+        <div className="about-text">
+          <p>
+            Welcome to Titanic Fitness, your premier online destination for fitness enthusiasts in West London. We are passionate about bringing the best of fitness to your fingertips, offering a wide range of resources and guidance to help you achieve your health and wellness goals. At Titanic Fitness, we understand the challenges of balancing a busy lifestyle with staying fit, which is why we provide accessible and effective solutions tailored to your needs. Whether you're a beginner or a seasoned fitness enthusiast, our platform offers a variety of workout plans, nutritional advice, and expert tips to support you on your fitness journey. Join us in creating a healthier and stronger you, right from the comfort of your own home.</p>
+        </div>
+        <br></br>
+        <div className="about-text">
+          <p>
+            At Titanic Fitness, our core values are the bedrock of our community. We believe in inclusivity, fostering a welcoming environment where everyone, regardless of their fitness level or background, feels empowered to embark on their wellness journey. We prioritize authenticity, ensuring that our resources and guidance are grounded in expertise and honesty, aiming to provide accurate and reliable information. Integrity is fundamental to our approach, as we strive to deliver transparent and ethical guidance, putting the well-being of our users at the forefront. At Titanic Fitness, we are committed to innovation, continuously exploring new methods and ideas to keep our offerings fresh, engaging, and effective. Our values shape everything we do, driving us to support and motivate individuals in West London on their path to a healthier lifestyle.</p>
+        </div>
+        <br></br>
+        <div>
+          <NavButton text="more about us" path="/about" />
+        </div>
+      
+
       </div>
-    </div>
+      </div>
+    
   );
 };
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -32,113 +32,122 @@ const Home = () => {
 
         <div>
 
-            <div className="background-image">
+            <div>
 
-                <div className="home-content">
+                <div className="background-image">
 
-                    <h1>OCEANS DESTIANTION</h1>
+                    <div className="home-content">
 
-                    <p>Welcome to Titanic Fitness!<br /> West Londons Very Own Gym group!</p>
+                        <h1>OCEANS DESTIANTION</h1>
 
-                    <NavButton text="about" path="/about" />
+                        <p>Welcome to Titanic Fitness!<br /> West Londons Very Own Gym group!</p>
 
-                    <NavButton text="exercises" path="/exercises" />
+                        <NavButton text="about" path="/about" />
 
-                </div>
-
-            </div>
-
-            <section className="service">
-
-                <div className="title">
-
-                    <h1>Our Services</h1>
-
-                    <div className="box">
-
-                        <div className="card">
-
-                            <i class="fas fa-user-friends"></i>
-
-                            <h4>Personal Training</h4>
-
-                            <div className="pra">
-
-                                <p>Are you looking for a someone to assist you while working out? Look at our personal trainer program today.</p>
-
-
-
-                                <a className="button" href="#">Read More</a>
-
-
-
-                            </div>
-
-
-
-
-                        </div>
-
-
-
-
-                        <div className="card">
-
-                            <i class="fas fa-user-friends"></i>
-
-                            <h4>Meal Plans</h4>
-
-                            <div className="pra">
-
-                                <p>If you aren't sure about the right type of meals you should be eating, don't worry! We have the right meal plan for everyone, even you!</p>
-
-
-
-                                <a className="button" href="#">Read More</a>
-
-
-
-                            </div>
-
-
-
-
-                        </div>
-
-
-
-
-                        <div className="card">
-
-                            <i class="fas fa-user-friends"></i>
-
-                            <h4>Online Sessions</h4>
-
-                            <div className="pra">
-
-                                <p>Every saturday and sunday we provide an online workout session caterd to those who want to workout as a group!
-
-                                    If you like working in a group, then come along!.</p>
-
-
-
-                                <a className="button" href="#">Read More</a>
-
-
-
-                            </div>
-
-
-
-
-                        </div>
+                        <NavButton text="exercises" path="/exercises" />
 
                     </div>
 
                 </div>
 
-            </section>
+                <section className="service">
 
+                    <div className="title">
+
+                        <h1>Our Services</h1>
+
+                        <div className="box">
+
+                            <div className="card">
+
+                                <i class="fas fa-user-friends"></i>
+
+                                <h4>Personal Training</h4>
+
+                                <div className="pra">
+
+                                    <p>Are you looking for a someone to assist you while working out? Look at our personal trainer program today.</p>
+
+
+
+                                    <a className="button" href="#">Read More</a>
+
+
+
+                                </div>
+
+
+
+
+                            </div>
+
+
+
+
+                            <div className="card">
+
+                                <i class="fas fa-user-friends"></i>
+
+                                <h4>Meal Plans</h4>
+
+                                <div className="pra">
+
+                                    <p>If you aren't sure about the right type of meals you should be eating, don't worry! We have the right meal plan for everyone, even you!</p>
+
+
+
+                                    <a className="button" href="#">Read More</a>
+
+
+
+                                </div>
+
+
+
+
+                            </div>
+
+
+
+
+                            <div className="card">
+
+                                <i class="fas fa-user-friends"></i>
+
+                                <h4>Online Sessions</h4>
+
+                                <div className="pra">
+
+                                    <p>Every saturday and sunday we provide an online workout session caterd to those who want to workout as a group!
+
+                                        If you like working in a group, then come along!.</p>
+
+
+
+                                    <a className="button" href="#">Read More</a>
+
+
+
+                                </div>
+
+
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </section>
+
+            </div>
+            <div className="links">
+                <a href="#">Terms and agreements</a>
+                <a href="#">About</a>
+                <a href="#">Contact Us</a>
+                <a href="#">Faq</a>
+            </div>
         </div>
 
 
