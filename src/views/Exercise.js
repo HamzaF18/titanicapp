3b63a98667c1e2902dd8a7f3d4c15fa27cb44430
HamzaF18 <40198734@hruc.ac.uk>
2023-11-10 16:42:33 +0000

    init

--- src/views/Exercise.js
+++ src/views/Exercise.js
@@ -4,9 +4,21 @@ import "./Exercise.css"
 const Exercise = ()=>{
     return(
         <div>
-            <div className="background-image">
+                <div className="background-image">
+                    <div className="exercise-intro">
+                    <h1>Exercise</h1>
+                    <p>There are many essential exercises that we provide in our gym!</p>
+                    </div>
+                    <br></br>
+                    <div className="exercise-cards">
+                        <img src="https://health.clevelandclinic.org/wp-content/uploads/sites/3/2022/04/exerciseHowOften-944015592-770x533-1.jpg"/>
+                        <img src="https://images.everydayhealth.com/images/strength-training-health-benefits-1440x810.jpg"/>
+
+                    </div>
+               
                 
-            </div>
+                </div>
+                
 
         </div>
     )
